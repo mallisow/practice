6ff9bcfcd60fb183dbf5bca0fa432a76204de756
mallisow <Pandu@LAPTOP-2QSP4N3J>
2021-07-05 22:36:19 +0530

    new code

---
+++ Practice1/src/Practice11.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Pandu
+ *
+ */
+public class Practice11 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Emp details are below");
+		System.out.println("Name : Malli");
+
+	}
+
+}
