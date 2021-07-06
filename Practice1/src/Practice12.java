2eb93d4752f2c59a26f7b4967144df7d020a7c91
mallisow <Pandu@LAPTOP-2QSP4N3J>
2021-07-06 08:35:06 +0530

    New program with company details

---
+++ Practice1/src/Practice12.java
@@ -0,0 +1,12 @@
+
+public class Practice12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Company details");
+		System.out.println("Atos Syntel");
+		
+
+	}
+
+}
